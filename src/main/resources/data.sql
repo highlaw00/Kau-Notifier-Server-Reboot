157b65c9eb9dee6194d7713cc9c55804e0278e25
highlaw00 <choiyool00@gmail.com>
2024-04-23 23:59:00 +0900

    Chore: SQL DDL, DML 추가
    기존에 존재하지 않던 FK 제약 조건문을 추가했습니다. 테스트 등에 사용될
    수 있도록 더미 데이터를 추가하였습니다.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+INSERT INTO `member`(`creation_timestamp`,`member_id`,`update_timestamp`,`email`,`name`) VALUES
+("2024-02-20 15:14:35",12,"2024-02-20 15:14:35","choiyool00@naver.com","최율");
+
+INSERT INTO `source`(`creation_timestamp`,`source_id`,`update_timestamp`,`description`,`name`,`url`) VALUES
+("2024-01-18 14:57:12",1,"2024-01-18 14:57:12","일반공지","normal_notice","https://kau.ac.kr/web/pages/gc32172b.do"),
+("2024-01-18 14:57:12",2,"2024-01-18 14:57:12","학사공지","college_nontice","https://kau.ac.kr/web/pages/gc14561b.do"),
+("2024-01-18 14:57:12",3,"2024-01-18 14:57:12","항공우주 및 기계공학부","aerospace_mechanical_engineering","http://college.kau.ac.kr/web/pages/gc1986b.do"),
+("2024-01-18 14:57:12",4,"2024-01-18 14:57:12","항공전자정보공학부","aerospace_electronics_engineering","http://college.kau.ac.kr/web/pages/gc23761b.do"),
+("2024-01-18 14:57:12",5,"2024-01-18 14:57:12","항공운항학과","aeronautical_flight","http://college.kau.ac.kr/web/pages/gc61682b.do"),
+("2024-01-18 14:57:12",6,"2024-01-18 14:57:12","항공교통물류학부","air_transporation_logistics","http://college.kau.ac.kr/web/pages/gc93464b.do"),
+("2024-01-18 14:57:12",7,"2024-01-18 14:57:12","경영학과","business_administration","http://college.kau.ac.kr/web/pages/gc99805b.do"),
+("2024-01-18 14:57:12",8,"2024-01-18 14:57:12","경영학부","faculty_of_business_administration","http://college.kau.ac.kr/web/pages/gc93337b.do"),
+("2024-01-18 14:57:12",9,"2024-01-18 14:57:12","신소재공학과","new_materials_engineering","http://college.kau.ac.kr/web/pages/gc46806b.do"),
+("2024-01-18 14:57:12",10,"2024-01-18 14:57:12","소프트웨어학과","software_engineering","http://college.kau.ac.kr/web/pages/gc911b.do"),
+("2024-01-18 14:57:12",11,"2024-01-18 14:57:12","스마트드론공학과","smart_drone_engineering","http://college.kau.ac.kr/web/pages/gc13106b.do"),
+("2024-01-18 14:57:12",12,"2024-01-18 14:57:12","자유전공학부 전계열","liberal_studies","http://college.kau.ac.kr/web/pages/gc46051b.do"),
+("2024-01-18 14:57:12",13,"2024-01-18 14:57:12","자유전공학부 공학계열","liberal_studies_engineering","http://college.kau.ac.kr/web/pages/gc16458b.do"),
+("2024-02-23 07:08:23",14,"2024-02-23 07:08:23","AI자율주행시스템공학과","ai_auto_pilot_engineering","http://college.kau.ac.kr/web/pages/gc33785b.do");
+
+INSERT INTO `subscription`(`creation_timestamp`,`member_id`,`source_id`,`subscription_id`) VALUES
+("2024-02-20 15:14:35",12,1,45),
+("2024-02-20 15:14:35",12,2,46),
+("2024-02-20 15:14:36",12,9,47),
+("2024-02-20 15:14:36",12,10,48),
+("2024-02-20 15:15:19",12,3,49),
+("2024-02-20 15:15:19",12,4,50),
+("2024-02-20 15:15:19",12,5,51),
+("2024-02-20 15:15:19",12,6,52),
+("2024-02-20 15:15:19",12,7,53),
+("2024-02-20 15:15:19",12,8,54),
+("2024-02-20 15:15:19",12,11,55),
+("2024-02-20 15:15:19",12,12,56),
+("2024-02-20 15:15:19",12,13,57);
